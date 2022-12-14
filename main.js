c9bd472d99f32d2a22cfc27c9ddfaa1b83acb265
kimsky <developer.kimsky@gmail.com>
2022-12-15 05:09:53 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -61,6 +61,13 @@ workBtnContainer.addEventListener("click", (event) => {
     return;
   }
 
+  // Remove selection from the previous item and select new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    event.target.nodeName === "BUTTON" ? event.target : event.target.parentNode;
+  target.classList.add("selected");
+
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
